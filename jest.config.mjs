d6f885f4fc9328054b079550fcf869ae6860213e
tkhongsap <ta.khongsap@gmail.com>
2025-05-24 07:44:13 +0700

    Add Jest testing framework and initial API tests

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  preset: 'ts-jest/presets/default-esm.js',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/client/src/$1',
+    '^@shared/(.*)$': '<rootDir>/shared/$1'
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      tsconfig: 'tsconfig.json'
+    }
+  }
+};
